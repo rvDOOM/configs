16af97fd12d2d650ae1c95a63497c1f21901cb80
rvDOOM <rvalerio9131@gmail.com>
2023-10-06 09:50:36 -0400

    Added preliminary waybar configs

--- symlinks.sh
+++ symlinks.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 
+# The directory where configs repo resides
 DIR=$(pwd)
 
 
-if [ -d ~/.config/hypr ]; then
-	if [ -L ~/.config/hypr ]; then
-		rm ~/.config/hypr
+# Hyprland symlink
+HYPRCONFIG=~/.config/hypr
+
+if [ -d $HYPRCONFIG ]; then
+	if [ -L $HYPRCONFIG ]; then
+		echo "Existing Hyprland config link. Removing."
+		rm $HYPRCONFIG
 	else
-		rm -rf ~/.config/hypr
+		echo "Existing Hyprland directory. Removing."
+		rm -rf $HYPRCONFIG
 	fi
 fi
 
+echo "Creating Hyprland symlink."
 ln -sfn $DIR/hypr/ ~/.config/
+
+
+# Waybar symlink
+WAYBARDIR=~/.config/waybar
+
+if [ -d $WAYBARDIR ]; then
+	if [ -L $WAYBARDIR ]; then
+		echo "Existing Waybar config link. Removing."
+		rm $WAYBARDIR
+	else
+		echo "Existing Waybar directory. Removing."
+		rm -rf $WAYBARDIR
+	fi
+fi
+
+echo "Creating Waybar symlink."
+ln -sfn $DIR/waybar/ ~/.config/
