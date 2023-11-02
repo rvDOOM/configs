0eb73a42c6f6b5b84972c7dcb73e891593ff7b69
rvDOOM <rvalerio9131@gmail.com>
2023-11-02 13:07:06 -0400

    Added more languages to nvim lsp congig

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -10,3 +10,36 @@ lspconfig.pyright.setup({
   capabilities = config.capabilities,
   filetypes={"python"}
 })
+
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = config.capabilities,
+  filetypes={"html"}
+})
+
+lspconfig.cssls.setup{
+  on_attach = on_attach,
+  capabilities = config.capabilities,
+  filetypes={"css"}
+}
+
+lspconfig.jsonls.setup{
+  on_attach = on_attach,
+  capabilities = config.capabilities,
+  filetypes={"json"}
+}
+
+lspconfig.tsserver.setup{
+  on_attach = on_attach,
+  capabilities = config.capabilities,
+  filetypes={"javascript"}
+}
+
+
+lspconfig.eslint.setup{
+  on_attach = on_attach,
+  capabilities = config.capabilities,
+  filetypes={"javascript"}
+}
+
+
